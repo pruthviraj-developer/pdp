3d6e51e85a09e6407d63719b1f41cc49f14f94a6
Pruthviraj.Patel <pruthviraj.patel@hopscotch.in>
2025-03-10 15:32:37 +0530

    Next.js version upgrade

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: '/batch', // The path on your app
+        destination: 'https://qa.hopscotch.in/batch', // The target server
+      },
+    ]
+  },
 };
 
 export default nextConfig;
